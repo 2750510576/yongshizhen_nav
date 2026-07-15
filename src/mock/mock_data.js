dea2d0e3bacb3803a634654755fe76f5d6e3cc7f
2750510576 <79034619+2750510576@users.noreply.github.com>
2026-07-16 00:15:52 +0800

    chore: 更新导航数据 - 2026/7/16 00:15:52

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -517,6 +517,7 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航🐱",
-  "search": "bing"
+  "title": "甬时珍 - 药店百宝箱",
+  "search": "bing",
+  "_fileSha": "354bdf3300f231848bfa8f1cf09ea8077e6f712f"
 }
