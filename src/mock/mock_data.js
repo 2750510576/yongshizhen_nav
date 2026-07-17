800c2e461997cb8054815f78194d38aa4449c6f4
2750510576 <79034619+2750510576@users.noreply.github.com>
2026-07-17 21:13:12 +0800

    chore: 更新导航数据 - 2026/7/17 21:13:13

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -45,8 +45,8 @@ export const mockData = {
           "id": "site-1784293847099",
           "name": "门店管理系统",
           "url": "https://manage.yongshizhen.top/",
-          "description": "集门店盘点货位管理一体",
-          "icon": ""
+          "description": "门店盘点货位拣货管理",
+          "icon": "https://img1.baidu.com/it/u=1897732871,2643484627&fm=253&fmt=auto&app=138&f=JPEG"
         },
         {
           "id": "site-1784132859742",
